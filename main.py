367eeee353c852e019175b10356a897ce5380cc7
祝贺 <2394241800@qq.com>
2023-05-27 10:18:40 +0800

    增加了输出结果的表示

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ import argparse
 import os
 
 import numpy as np
-from PIL import Image,ImageDraw,ImageFont,ExifTags
+from PIL import Image,ImageDraw,ImageFont,ImageOps
 from skimage import io
-
 
 import sys
 sys.path.append('./HandwrittingRecognition/')
@@ -15,11 +14,10 @@ RecModule = importlib.import_module(".Rec","HandwrittingRecognition")
 DetModule = importlib.import_module(".Det","CRAFT-pytorch")
 
 parser = argparse.ArgumentParser(description='HWCR arguments')
-parser.add_argument('--image_path', dest='image_path', default='./asserts/jiangnan.jpg', type=str, help='image for Dec&Rec')
+parser.add_argument('--image_path', dest='image_path', default='./asserts/B8H.png', type=str, help='image for Dec&Rec')
 parser.add_argument('--font_path', dest='font_path', default='./fonts/DENG.ttf', type=str, help='font for Dec&Rec')
 parser.add_argument('--result_root', dest='result_root', default='./results/', type=str, help='results root')
-parser.add_argument('--show_time', dest='show_time', default=True, type=bool, help='show time or not')
-parser.add_argument('--debug',dest='debug',default=False,type=bool, help='print heatmap when in debug mode')
+parser.add_argument('--debug',dest='debug',default=True,type=bool, help='print heatmap when in debug mode')
 args = parser.parse_args()
 print(args)
 DetModel = './CRAFT-pytorch/craft_mlt_25k.pth'
@@ -28,42 +26,48 @@ RecIndex = './HandwrittingRecognition/label2cha.json'
 
 # init
 
-Det = DetModule.Detection(model_path=DetModel, show_time=args.show_time)
+Det = DetModule.Detection(model_path=DetModel, show_time=args.debug)
 Rec = RecModule.Recognition(model_path=RecModel,index_path=RecIndex)
 
 # load image
 image = Image.open(args.image_path) # RGB order
-try:
-    for orientation in ExifTags.TAGS.keys() : 
-        if ExifTags.TAGS[orientation]=='Orientation' : break 
-    exif=dict(image._getexif().items())
-    if   exif[orientation] == 3 : 
-        image=image.rotate(180, expand = True)
-    elif exif[orientation] == 6 : 
-        image=image.rotate(270, expand = True)
-    elif exif[orientation] == 8 : 
-        image=image.rotate(90, expand = True)
-except:
-    pass
+image = ImageOps.exif_transpose(image)
 image = image.convert('RGB')
 
+dirname = str.split(args.image_path,'/')[-1].split('.')[0]+'/'
+if not os.path.exists(os.path.join(args.result_root,dirname)):
+    os.makedirs(os.path.join(args.result_root,dirname))
+
 board = Image.new(mode='RGB',size=image.size, color='white')
-board.paste(image,(0,0))
 image_array = np.asarray(image)
 
 boxes = Det.detect(image_array)
 bounds = [(box[0][0],box[0][1],box[2][0],box[2][1]) for box in boxes]
 
 text_regions = [image.crop(rec) for rec in bounds]
-predictions = Rec.recognise(text_regions)
+processed_imgs,predictions = Rec.recognise(text_regions)
 
-draw = ImageDraw.Draw(board)
+draw_on_image = ImageDraw.Draw(image)
+draw_on_board = ImageDraw.Draw(board)
 font = ImageFont.truetype(font=args.font_path, size=20)
+
 for i in range(len(boxes)):
     width = int(boxes[i][2][0] - boxes[i][0][0])
-    draw.rectangle(bounds[i],fill=None,outline=(0,0,255),width=1)  
-    draw.text(xy=tuple(boxes[i][0]),text=predictions[i],fill=(0,0,255), 
+    draw_on_image.rectangle(bounds[i],fill=None,outline=(0,0,255),width=1)  
+    draw_on_board.text(xy=tuple(boxes[i][0]),text=predictions[i][0],fill=(0,0,255), 
               font=(ImageFont.truetype(font=args.font_path, size=width)))
+    if args.debug:
+        region_dir = os.path.join(args.result_root,dirname,f'{i}/')
+        if not os.path.exists(region_dir):
+            os.makedirs(region_dir)
+        text_regions[i].save(os.path.join(region_dir,"raw.png").replace('\\','/'))
+        processed_imgs[i].save(os.path.join(region_dir,"processed.png").replace('\\','/'))
+        with open(os.path.join(region_dir,"prediction.txt"),'w',encoding='utf-8') as f:
+            f.write(str(predictions[i]))
 
-filename = str.split(args.image_path,'/')[-1]
-board.save(os.path.join(args.result_root,filename).replace('\\','/'))
+w,h = image.size
+output = Image.new(mode='RGB',size=(w*2,h), color='white')
+output.paste(image,(0,0))
+output.paste(board,(w,0))
+
+output.save(os.path.join(args.result_root,dirname,"output.png").replace('\\','/'))
