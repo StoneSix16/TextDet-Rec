f48203cd64355282498201d1b0c9f77f97e455a1
祝贺 <2394241800@qq.com>
2023-07-19 14:22:20 +0800

    添加说明

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # TextDet-Rec
-这是一个用于文本检测和识别的程序
+这是一个用于文本检测和识别的程序，可以检测和识别场景中的文字（包括3000+汉字，数字，字母和部分符号）
+该程序可以分为文本检测和字符识别两个部分，每个部分都通过深度神经网络实现。其中，文本检测部分采用模型CRAFT，字符识别部分采用模型EfficientNet
+## 程序模块介绍
+### 文本检测模型 CRAFT
+[CRAFT](https://arxiv.org/abs/1904.01941)是一个文本检测模型，通过VGG16+UNet架构下的神经网络输出一张表示像素点属于字符区域或字符间区域的可能性的热力图，并根据该图来分割出文本区域。
+![](./assertsForREADME/CRAFT_ex.png)
+由于CRAFT模型可以输出一张表达字符区域的热力图，该输出也可以用来分割字符区域。将分割出的字符区域作为字符识别模型的输入，就可以获得字符识别的结果。
+CRAFT预训练模型已经开源至[github](https://github.com/clovaai/CRAFT-pytorch)，对代码的后处理部分做部分修改和封装后，该模型可以达到我们的要求。
+### 字符识别模型 EfficientNet
+[EfficientNet]()是谷歌发布的一个CNN神经网络模型，通过对神经网络宽度，深度和输入图片分辨率组合的不断调整得到，可以快速准确的运行图片分类工作。
+为了实现字符识别的功能，我使用了[中科大的手写汉字数据集](http://www.nlpr.ia.ac.cn/databases/handwriting/Home.html)HWDB1.1，包含3755个字符。
+![](./assertsForREADME/HWDB_ex.png)
+最终的字符识别模型在HWDB训练集上迭代了45次，在HWDB测试集上的准确率达到了92.3%
+### 实现细节
+为了能够识别自然场景下的文本，EfficientNet模型的输入是RGB三通道图片。但HWDB数据集是单通道图片且背景颜色统一，在训练时，我通过PIL库直接将单通道图片转成三通道图片，这种转化有一定的效果，但也导致模型难以准确识别背景颜色复杂的文字。因此，我将CRAFT输出的图片先转化成单通道图片，进行一些二值化处理后再传给EfficientNet，这比较好地解决的问题。 
+程序接受一张图片后，会输出一张框出了字符区域并写出相应预测字符的图片。
+## 程序运行方法
+程序中含有4个参数: 
+* image_path：输入图片的路径，需要输入main.py的相对路径，默认为``./asserts/poem.jpg``
+* font_path：输出图片上预测字符的字体，需要输入main.py的相对路径，默认为``./fonts/DENG.ttf``
+* result_root：输出图片所在的目录，需要输入main.py的相对路径，默认为``./results/``
+* debug：打印调试信息，会在控制台输出调试信息，并在输出图片时额外输出每个单独文字区域的原图像，处理后图像，预测字符排序，默认为``True``
+
+要运行程序，请在TextDet-Rec目录下运行指令``python main.py --image_path==./asserts/<your img>``。输出结果会保存在``./results/<your img>/``目录下
+
+## 实验效果
+目前，该项目可以分割出图片中的字符区域并尝试预测，对白色背景下的汉字字符识别准确率较高。但由于是脱机识别，且类别数过多，存在一些识别错误的现象。
+![](./assertsForREADME/example1.png)
+![](./assertsForREADME/example2.png)
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import importlib 
 import argparse
 import os
+import torch
+import time
 
 import numpy as np
 from PIL import Image,ImageDraw,ImageFont,ImageOps
@@ -14,20 +16,33 @@ RecModule = importlib.import_module(".Rec","HandwrittingRecognition")
 DetModule = importlib.import_module(".Det","CRAFT-pytorch")
 
 parser = argparse.ArgumentParser(description='HWCR arguments')
-parser.add_argument('--image_path', dest='image_path', default='./asserts/B8H.png', type=str, help='image for Dec&Rec')
+parser.add_argument('--image_path', dest='image_path', default='./asserts/poem.jpg', type=str, help='image for Dec&Rec')
 parser.add_argument('--font_path', dest='font_path', default='./fonts/DENG.ttf', type=str, help='font for Dec&Rec')
 parser.add_argument('--result_root', dest='result_root', default='./results/', type=str, help='results root')
 parser.add_argument('--debug',dest='debug',default=True,type=bool, help='print heatmap when in debug mode')
+
 args = parser.parse_args()
-print(args)
+# when execute as .exe, the path should be relative
+if args.image_path[0]=='.':
+    args.image_path = os.path.dirname(__file__)+args.image_path[1:]
+if args.font_path[0]=='.':
+    args.font_path = os.path.dirname(__file__)+args.font_path[1:]
+if args.result_root[0]=='.':
+    args.result_root = os.path.dirname(__file__)+args.result_root[1:]
+
 DetModel = './CRAFT-pytorch/craft_mlt_25k.pth'
-RecModel = './HandwrittingRecognition/efficientnet_20.pth'
+RecModel = './HandwrittingRecognition/efficientnet_45.pth'
 RecIndex = './HandwrittingRecognition/label2cha.json'
 
 # init
 
-Det = DetModule.Detection(model_path=DetModel, show_time=args.debug)
-Rec = RecModule.Recognition(model_path=RecModel,index_path=RecIndex)
+Det = DetModule.Detection(model_path=DetModel)
+Rec = RecModule.Recognition(model_path=RecModel, index_path=RecIndex)
+
+if torch.cuda.is_available():
+    print(f'the net work will run on GPU')
+else:
+    print(f'the net work will run on CPU')
 
 # load image
 image = Image.open(args.image_path) # RGB order
@@ -41,11 +56,20 @@ if not os.path.exists(os.path.join(args.result_root,dirname)):
 board = Image.new(mode='RGB',size=image.size, color='white')
 image_array = np.asarray(image)
 
+t0 = time.time()
 boxes = Det.detect(image_array)
+t0 = time.time() - t0
+print(f'detection finished with time {t0}')
+
+
 bounds = [(box[0][0],box[0][1],box[2][0],box[2][1]) for box in boxes]
 
 text_regions = [image.crop(rec) for rec in bounds]
+
+t1 = time.time()
 processed_imgs,predictions = Rec.recognise(text_regions)
+t1 = time.time() - t1
+print(f'recognition finished with time {t1}')
 
 draw_on_image = ImageDraw.Draw(image)
 draw_on_board = ImageDraw.Draw(board)
